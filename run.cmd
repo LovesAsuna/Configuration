8bd5537b88e538f34de5af1ca9062e550c7b7e00
LovesAsuna <625924077@qq.com>
2019-08-19 22:45:20 +0800

    update

--- run.cmd
+++ run.cmd
@@ -6,9 +6,7 @@ rem 导入自身位置
 set file=%0 & set file=!file:run.cmd=! & set file=!file:"=! 
 set file=%file: =%
 rem 识别是否为Bungeecord核心
-if exist Bungeecord.jar set jar=BungeeCord.jar && set server=BungeeCord && set status=bc && for /f "tokens=3 delims=:" %%i in ('findstr /rc:"\<host\>" config.yml') do (
-  set port=%%i
-) &&  goto Bungeecord
+if exist BungeeCord.jar set jar=BungeeCord.jar && set server=BungeeCord && set status=bc && goto BungeeCord
 rem 读取服务端核心
 if not exist eula.txt title Default控制端 && set status=spigot && goto firstrun   
 goto reload
@@ -27,7 +25,10 @@ for /f "tokens=4" %%i in ('dir') do (
 echo Max=%max%>>eula.txt & echo Min=%min%>>eula.txt
 goto reload
 
-:Bungeecord
+:BungeeCord
+for /f "tokens=3 delims=:" %%i in ('findstr /rc:"\<host\>" config.yml') do (
+  set port=%%i 
+)
 if not exist %file%modules\settings.ini goto setbcmemory
 for /f "tokens=2 delims==" %%i in (%file%modules\settings.ini) do (
 		if "!times!"=="1" set max=%%i
@@ -36,7 +37,8 @@ for /f "tokens=2 delims==" %%i in (%file%modules\settings.ini) do (
 ) 
 goto reloadnode
 :setbcmemory
-echo 检测到为Bungeecord模式 & title %server%控制端 & echo 请设置最大内存和最小内存!
+cls & set port=25577
+echo 检测到为Bungeecord模式且未经初始化 & title %server%控制端 & echo 请设置最大内存和最小内存!
 set /p max=最大内存(Max):
 set /p min=最小内存(Min):
 md %file%modules & cls
@@ -60,7 +62,7 @@ for /f "tokens=2 delims==" %%i in ('findstr /rc:"\<server-port\>" server.propert
 )
 :reloadnode
 title %server%控制端^|端口号:%port%(重启次数=%reload%)^|Max=%max%^|Min=%min%
-java -Xmx%max% -Xms%min% -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar %jar%
+java -Xmx%max% -Xms%min% -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dfile.encoding=UTF-8 -Dusing.aikars.flags=mcflags.emc.gs -jar %jar%
 ping /n 1 127.0.0.1>nul & cls
 choice /c cr /n /t 4 /d r /m 输入c即可关闭窗口！& cls
 
